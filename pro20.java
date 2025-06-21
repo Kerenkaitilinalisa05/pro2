43c77ff98f0c5f9a57dce170d98674878ff4575e
Kerenkaitilinalisa05 <24203020@srcas.ac.in>
2025-06-21 11:57:17 +0530

    Update pro20.java

--- pro20.java
+++ pro20.java
@@ -1,5 +1,5 @@
 public class pro20{
-  public staticc void main (String[] args){
+  public static void main (String[] args){
     system.out.println("Welcome");
     system.out.println("to");
     system.out.println("Devops");
